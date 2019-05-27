948fd441730e0fa804b0a368b87795f857fb6769
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-27 21:05:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,6 +79,7 @@ holiday_hash.each do |season, data|
     puts string 
   end
 end
+end 
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
